e8573c068bf2c5ec7b2916fa4bb83bd8977ac8c7
Amr Eshra <amr.eshra@gmail.com>
2020-06-21 14:41:45 +0300

    .

--- sales-project/src/app/app.module.ts
+++ sales-project/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
+/* Any New Component should be imported here */
 import { AppComponent } from './app.component';
 import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+/* Any New Services should be imported here */
+import {ProductService} from './product-list/product.service';
 
 @NgModule({
   declarations: [
+    /* Add the declaration of Components */
     AppComponent,
-    SalesPersonListComponent
+    SalesPersonListComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    /* Add the angular modules */
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+
+  /* Add the service providers */
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ sales-project/src/app/product-list/product.ts
@@ -0,0 +1,6 @@
+export class Product {
+
+	constructor(public sku: string,	public name: string, public description: string, public unitPrice: number,
+		public imageUrl: string, public active: boolean, public unitsInStock: number, public createDate: Date,
+		public updateDate: Date, public categoryId: number){}
+}
