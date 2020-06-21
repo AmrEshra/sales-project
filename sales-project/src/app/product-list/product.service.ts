f50e74989cf8c8a21125c02f8087af51f2823ed9
Amr Eshra <amr.eshra@gmail.com>
2020-06-21 17:15:58 +0300

    .

--- sales-project/src/app/product-list/product.service.ts
+++ sales-project/src/app/product-list/product.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseurl = 'http://localhost:8005/ECommerce/product-api/Products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]>{
+
+    return this.httpClient.get<GetResponse>(this.baseurl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
+  getProductsList() {
+    return this.httpClient.get<Product[]>(this.baseurl)
+        .pipe(
+            map(
+                (products) => {
+                    console.log(products);
+                    return products;
+                }
+            )
+        );
+  }
+}
+
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
