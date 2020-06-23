91a14792f0912c18a0e13a46a5736ee28ffb423f
Amr Eshra <amr.eshra@gmail.com>
2020-06-23 10:41:49 +0300

    .

--- sales-project/src/app/app.module.ts
+++ sales-project/src/app/app.module.ts
@@ -1,6 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { UiSwitchModule } from 'ngx-toggle-switch';
+import { ToastModule } from 'primeng/toast';
+import { DropdownModule } from 'primeng/dropdown';
+import {MultiSelectModule} from 'primeng/multiselect';
+import { StepsModule } from 'primeng/steps';
+import { TabViewModule } from 'primeng/tabview';
+import 'zone.js/dist/zone';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 /* Any New Component should be imported here */
 import { AppComponent } from './app.component';
@@ -9,22 +28,53 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 /* Any New Services should be imported here */
 import {ProductService} from './product-list/product.service';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @NgModule({
   declarations: [
-    /* Add the declaration of Components */
     AppComponent,
     SalesPersonListComponent,
     ProductListComponent
   ],
   imports: [
-    /* Add the angular modules */
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    MultiSelectModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    FormsModule,
+    ButtonModule,
+    TableModule,
+    UiSwitchModule,
+    BrowserAnimationsModule,
+    TableModule,
+    PanelMenuModule,
+    DialogModule,
+    MessagesModule,
+    ToastModule,
+    DropdownModule,
+    NgxSpinnerModule,
+    StepsModule,
+    TabViewModule,
+    ConfirmDialogModule
   ],
-
-  /* Add the service providers */
-  providers: [ProductService],
+  providers: [
+    TranslateService,
+    MessageService,
+    ConfirmationService,
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
